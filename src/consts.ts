7be6e183777cdbcac4d0995eebf058ff10f37c00
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-06-27 17:01:03 +0900

    first

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "場作りブログ";
+export const SITE_DESCRIPTION = "ゆるゆると場を作る様子を記録するWebサイトです";
