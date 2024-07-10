a9c656fe5cce9386a5431558937144559463a764
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-10 15:36:05 +0900

    一通り追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import auth from "auth-astro";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.tsum.ug",
-  integrations: [mdx(), sitemap(), react()],
-  vite: { optimizeDeps: { exclude: ["@resvg/resvg-js"] } },
+  integrations: [mdx(), sitemap(), react(), auth()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  output: "server",
+  adapter: cloudflare(),
 });
