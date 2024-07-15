a5c90b54360cd0fde015e88da36f37d39efebdae
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 01:30:13 +0900

    デバッグ機能を追加

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -3,6 +3,13 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 function GoogleLoginButton() {
   return (
     <div>
+      <button
+        onClick={() => {
+          console.log(import.meta.env.PUBLIC_GOOGLE_CLIENT_ID);
+        }}
+      >
+        CLIENT_ID
+      </button>
       <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
         <GoogleLogin
           onSuccess={(credentialResponse) => {
