bf1fd2dd05d84635b104573330d92592aa2f3e69
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 02:01:32 +0900

    APIをやめて、CORSを回避するようにしてみる

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -10,9 +10,21 @@ function GoogleLoginButton() {
       <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
         <GoogleLogin
           onSuccess={async (credentialResponse) => {
-            const userInfo = await fetch(
-              `https://${SITE_ORIGIN}/userinfo/userinfo.json?access_token=${credentialResponse.credential}`
-            );
+            const userInfo = await new Promise((resolve) => {
+              const xhr = new XMLHttpRequest();
+
+              xhr.open("GET", `https://www.googleapis.com/oauth2/v3/userinfo`);
+              xhr.setRequestHeader(
+                "Authorization",
+                `Bearer ${credentialResponse.credential}`
+              );
+              xhr.onload = function () {
+                if (this.status >= 200 && this.status < 300)
+                  resolve(JSON.parse(this.responseText));
+                else resolve({ err: "404" });
+              };
+              xhr.send();
+            });
             console.log(credentialResponse, userInfo);
             //setCookie("google-cledentials", credentialResponse);
           }}
