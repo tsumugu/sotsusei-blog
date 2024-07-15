77a2f14cd0c9632228981a04883acaa30ba418bd
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 03:34:51 +0900

    タイポを修正

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -2,17 +2,16 @@ import axios from "axios";
 
 export const handler = async (event, context) => {
   const query_access_token = event["queryStringParameters"]["access_token"];
+  const res = await axios.get(
+    `https://oauth2.googleapis.com/tokeninfo?id_token=${query_access_token}`
+  );
   const response = {
     statusCode: 200,
     headers: {
       "Content-Type": "text/html",
       "Access-Control-Allow-Origin": "*",
     },
-    body: JSON.stringify(
-      await axios.get(
-        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${query_access_token}`
-      ).data
-    ),
+    body: JSON.stringify(res.data),
   };
   return response;
 };
