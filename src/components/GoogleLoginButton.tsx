6338f14f826a107249057c63f5f0da4231c634d6
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 04:03:43 +0900

    ログインのチェック

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,30 +1,41 @@
-//import { CookiesProvider, useCookies } from "react-cookie";
+import { CookiesProvider, useCookies } from "react-cookie";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { SITE_ORIGIN } from "../consts";
 
 function GoogleLoginButton() {
-  //const [_, setCookie] = useCookies(["google-cledentials"]);
+  const [_, setCookie] = useCookies(["isTamabiUser"]);
   return (
     <div>
-      {/* <CookiesProvider defaultSetOptions={{ path: "/" }}> */}
-      <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
-        <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            const fetchUserInfo = async () => {
-              const userInfo = await fetch(
-                `https://wribrm1pf4.execute-api.ap-northeast-1.amazonaws.com/default/verifyGoogleCredential?access_token=${credentialResponse.credential}`
-              );
-              console.log(userInfo.json());
-              //setCookie("google-cledentials", credentialResponse);
-            };
-            fetchUserInfo();
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-      </GoogleOAuthProvider>
-      {/* </CookiesProvider> */}
+      <CookiesProvider defaultSetOptions={{ path: "/" }}>
+        <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              const fetchUserInfo = async () => {
+                const userInfo = await (
+                  await fetch(
+                    `https://wribrm1pf4.execute-api.ap-northeast-1.amazonaws.com/default/verifyGoogleCredential?access_token=${credentialResponse.credential}`
+                  )
+                ).json();
+                const isTamabiUser = userInfo.email.match(
+                  /^.*@(?:.*\.)?tamabi\.ac\.jp$/gm
+                );
+                if (isTamabiUser) {
+                  setCookie(
+                    "isTamabiUser",
+                    userInfo.email.match(/^.*@(?:.*\.)?tamabi\.ac\.jp$/gm)
+                  );
+                } else {
+                  alert("多摩美のメールアドレスでログインしてください");
+                  console.log(userInfo.email);
+                }
+              };
+              fetchUserInfo();
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      </CookiesProvider>
     </div>
   );
 }
