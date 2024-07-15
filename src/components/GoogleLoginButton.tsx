1ca93af2943373ae06f3b3f4e7564094b67b946c
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 01:24:51 +0900

    環境変数のprefixをPUBLIC_にした

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,9 +1,9 @@
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
-function LoginUserForm() {
+function GoogleLoginButton() {
   return (
     <div>
-      <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+      <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
         <GoogleLogin
           onSuccess={(credentialResponse) => {
             console.log(credentialResponse);
@@ -17,4 +17,4 @@ function LoginUserForm() {
   );
 }
 
-export default LoginUserForm;
+export default GoogleLoginButton;
