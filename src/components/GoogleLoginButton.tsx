1eb2b782041f7630c7906ffc5f1c80f9f09d4daf
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-07-16 01:43:08 +0900

    ユーザ情報を取得する処理を実装

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,25 +1,26 @@
+//import { CookiesProvider, useCookies } from "react-cookie";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 function GoogleLoginButton() {
+  //const [_, setCookie] = useCookies(["google-cledentials"]);
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log(import.meta.env.PUBLIC_GOOGLE_CLIENT_ID);
-        }}
-      >
-        CLIENT_ID
-      </button>
+      {/* <CookiesProvider defaultSetOptions={{ path: "/" }}> */}
       <GoogleOAuthProvider clientId={import.meta.env.PUBLIC_GOOGLE_CLIENT_ID}>
         <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
+          onSuccess={async (credentialResponse) => {
+            const userInfo = await fetch(
+              `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${credentialResponse.credential}`
+            );
+            console.log(credentialResponse, userInfo);
+            //setCookie("google-cledentials", credentialResponse);
           }}
           onError={() => {
             console.log("Login Failed");
           }}
         />
       </GoogleOAuthProvider>
+      {/* </CookiesProvider> */}
     </div>
   );
 }
