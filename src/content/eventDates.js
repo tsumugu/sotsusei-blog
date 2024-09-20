cbc9eb92e4343bfb0b1089696279ed62158f0176
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-09-20 16:13:53 +0900

    9/10の活動記録を追加

--- src/content/eventDates.js
+++ src/content/eventDates.js
@@ -2,4 +2,5 @@ export const eventDates = {
   "2024-07-03": { startedAt: "13:10", endedAt: "15:00" },
   "2024-07-09": { startedAt: "13:10", endedAt: "16:30" },
   "2024-07-17": { startedAt: "13:10", endedAt: "17:15" },
+  "2024-09-10": { startedAt: "13:30", endedAt: "15:30" },
 };
