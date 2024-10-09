c3c534f33c2da41cb6777c94fdcdf0789c8132d9
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-10-09 16:21:49 +0900

    マップを埋め込みのOpenStreetMapに変更

--- src/components/Blog/PostBasicInfo/TamabiMap/TamabiMap.jsx
+++ src/components/Blog/PostBasicInfo/TamabiMap/TamabiMap.jsx
@@ -5,28 +5,17 @@ import { FaMapMarker } from "react-icons/fa";
 const TamabiMap = ({ place }) => {
   const pos = {
     東学広場: {
-      topPer: "56%",
-      leftPer: "45%",
+      lat: "35.610389544636156",
+      lon: "139.34909999370575",
     },
   };
   return (
     <div className="map-wrapper">
-      <div
-        className="pin"
-        style={{ top: pos[place].topPer, left: pos[place].leftPer }}
-      >
-        <FaMapMarker size={38} color="red" />
-        <p>{place}</p>
-      </div>
-      <img
-        className="map"
-        src="https://d3djg0dm3dsv8.cloudfront.net/bf12c64105970d933785b7b9f311ddaf26293dedda0d3d6a3b17d4140c86c409.png"
-      />
-      <p className="credit">
-        © OpenStreetMap contributors
-        <a href="https://www.openstreetmap.org/copyright">License</a>
-        <a href="https://www.opendatacommons.org/licenses/odbl">Terms</a>
-      </p>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=139.3464767932892%2C35.608241602118504%2C139.35172319412234%2C35.61253742948412&amp;layer=mapnik&amp;marker=35.610389544636156%2C139.34909999370575"
+      ></iframe>
     </div>
   );
 };
