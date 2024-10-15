b6a2eecc57831903229f791895f86ca01bec37f9
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-10-15 10:01:49 +0900

    マップをマイマップにした

--- src/components/Blog/PostBasicInfo/TamabiMap/TamabiMap.jsx
+++ src/components/Blog/PostBasicInfo/TamabiMap/TamabiMap.jsx
@@ -3,18 +3,12 @@ import "./TamabiMap.css";
 import { FaMapMarker } from "react-icons/fa";
 
 const TamabiMap = ({ place }) => {
-  const pos = {
-    東学広場: {
-      lat: "35.610389544636156",
-      lon: "139.34909999370575",
-    },
-  };
   return (
     <div className="map-wrapper">
       <iframe
+        src="https://www.google.com/maps/d/u/0/embed?mid=1P4E-AcbiUfkGL585h7fAkSMZOGzMafA&ehbc=2E312F&z=17"
         width="100%"
         height="100%"
-        src="https://www.openstreetmap.org/export/embed.html?bbox=139.3464767932892%2C35.608241602118504%2C139.35172319412234%2C35.61253742948412&amp;layer=mapnik&amp;marker=35.610389544636156%2C139.34909999370575"
       ></iframe>
     </div>
   );
