ca7c5fa22e4f49c0247b13c16910fc9fdf346751
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-11-14 12:25:20 +0900

    時間を追加

--- src/content/eventDates.js
+++ src/content/eventDates.js
@@ -1,4 +1,5 @@
 export const eventDates = {
+  "2024-05-29": { startedAt: "14:00", endedAt: "15:00", durMinutes: 60 },
   "2024-07-03": { startedAt: "13:10", endedAt: "15:00", durMinutes: 110 },
   "2024-07-09": { startedAt: "13:10", endedAt: "16:30", durMinutes: 200 },
   "2024-07-17": { startedAt: "13:10", endedAt: "17:15", durMinutes: 245 },
