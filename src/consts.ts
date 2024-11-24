00cfff8231527f829b82cc56a57e476d90019477
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-11-24 21:08:12 +0900

    ロゴマークの追加、OGPの調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "場作りブログ";
+export const SITE_TITLE = "TSUMUGU FREE COFFEE";
 export const SITE_DESCRIPTION = "卒業制作で場を作る様子を記録するWebサイトです";
 export const SITE_ORIGIN = "blog.tsum.ug";
 
