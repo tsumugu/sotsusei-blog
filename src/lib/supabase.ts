466758662b04e2edb36a1e19088277fdbbfdbf33
tsumugu <tsumugu1515@gmail.com>
2025-03-06 18:17:26 +0900

    supabaseのキーを変更

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://wxrfsbidutyxjfjrctsa.supabase.co/",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4cmZzYmlkdXR5eGpmanJjdHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0OTk2NzIsImV4cCI6MjA0MTA3NTY3Mn0.b5NzP59Y0Ot1L2FH3AC1KKIlD3SJXp4x7TlgK1F5PPc"
+  "https://dqkqivfaialvrrfsslaf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxa3FpdmZhaWFsdnJyZnNzbGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyNTE3NzAsImV4cCI6MjA1NjgyNzc3MH0.kDf7lPNZEPrEcfK1MwmLBM-5BjJ3sII98fYUO1DtbEc"
 );
